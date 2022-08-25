1d3ce75a8b7750cf0ea384c0120a5e6b0cb91dfb
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-25 10:25:14 +0000

    [Finish応用課題６非同期通信]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,22 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
+    @user = @book.user
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     if @comment.save
-      redirect_to request.referer
     else
-      redirect_to request.referer
+      @book_comment = BookComment.new
+      render 'books/show'
     end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+
     @comment = BookComment.find(params[:id])
     @comment.destroy
-    redirect_to request.referer
   end
 
   private
