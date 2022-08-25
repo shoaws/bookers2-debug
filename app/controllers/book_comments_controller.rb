d6febc224f4681c719bf594820fdb39232a760c2
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-25 08:39:18 +0000

    [Addコメント機能修正]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,9 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
       redirect_to request.referer
     else
       redirect_to request.referer
@@ -11,9 +11,8 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book_id: book.id)
-    comment.destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
     redirect_to request.referer
   end
 
