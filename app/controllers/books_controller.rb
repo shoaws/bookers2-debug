12b93afa464a1afe5800e3a6f57f5cf5ea47988d
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-01 10:18:47 +0000

    [Add閲覧数表示]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,20 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
+
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
   end
 
   def index
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
     @books = Book.includes(:favorited_users).
-      sort_by{|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
+      sort{|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
 
     @book = Book.new
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
 
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
+  
+  has_many :view_counts, dependent: :destroy
 
   has_one_attached :profile_image
 
